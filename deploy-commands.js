017075ee42246d1990fe86dcbfcd88fdbeecc4a7
cargot1227 <cargot1227@gmail.com>
2026-04-18 23:02:20 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 const commands = [
-
   new SlashCommandBuilder()
   .setName('setup-tickets')
   .setDescription('Envoie le message de tickets dans le salon 🎫'),
